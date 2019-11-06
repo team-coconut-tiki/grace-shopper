a3ef4732db0f0fe539d53049c9ec485556c55cb8
Jasmine Wang <jasminey.tea@gmail.com>
2019-11-06 17:36:42 -0600

    components now work with router, and show up

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,7 +1,6 @@
-
 import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {getAllProducts} from '../store/allproducts'
+import {getAllProducts} from '../store/allProducts'
 
 export const ProductsList = () => {
   const products = useSelector(state => state.products)
