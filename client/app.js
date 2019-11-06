f02885eaa397dd50ff82425257690634384bcfd2
randy <randydxp@gmail.com>
2019-11-06 11:41:06 -0600

    Changed db name and cart item to use underscores and added frontend component and GET thunk

--- client/app.js
+++ client/app.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import SingleUser from './components/SingleUser'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <SingleUser />
     </div>
   )
 }
