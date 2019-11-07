8d2c4eddc5ea8cfd9a9f0e01b170f09f4580d739
randy <randydxp@gmail.com>
2019-11-07 15:16:40 -0600

    fixed re-route to go to products page

--- client/routes.js
+++ client/routes.js
@@ -38,7 +38,7 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/" component={UserHome} />
+            <Route path="/" component={AllProducts} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
