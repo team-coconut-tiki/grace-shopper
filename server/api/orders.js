266da1547886c04c3b2f364f4419ec77d8900f7e
Chris Pietka <chrispietka@yahoo.com>
2019-11-09 15:51:11 -0600

    added single order route

--- server/api/orders.js
+++ server/api/orders.js
@@ -10,3 +10,12 @@ router.get('/', async (req, res, next) => {
     next(error)
   }
 })
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.id)
+    res.json(order)
+  } catch (error) {
+    next(error)
+  }
+})
