c447666f4070ffd72265987d4897849caeee3a47
Chris Pietka <chrispietka@yahoo.com>
2019-11-12 16:41:18 -0600

    commented out update password form component

--- client/components/UpdatePasswordForm.js
+++ client/components/UpdatePasswordForm.js
@@ -1,9 +1,29 @@
-import React from 'react'
-import {useDispatch} from 'react-redux'
-import {} from '../store/'
+// import React from 'react'
+// import {useDispatch} from 'react-redux'
+// import {updateUserThunk} from '../store/currentUser'
 
-const UpdatePasswordForm = props => {
-  return <div>Reset your password</div>
-}
+// const UpdatePasswordForm = props => {
+//   const dispatch = useDispatch()
+//   const HandleSubmit = evt => {
+//     const password = evt.target.password.value
+//     dispatch(updateUserThunk({password}))
+//   }
+//   return (
+//     <>
+//       <div>woooOOOOoooo</div>
+//       <div>
+//         <form onSubmit={HandleSubmit}>
+//           <div>
+//             <label htmlFor="Enter new password">
+//               <small>Enter new password</small>
+//             </label>
+//             <input name="password" type="password" />
+//             <button type="submit">Submit</button>
+//           </div>
+//         </form>
+//       </div>
+//     </>
+//   )
+// }
 
-export default UpdatePasswordForm
+// export default UpdatePasswordForm
